6ff36fce5d081da84a80f9a740c43a6fa2bd8c4b
nog <nogueira@pm.ba.gov.br>
2020-06-01 20:41:37 -0300

    implementando as rotas

---
+++ src/pages/Cart/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Cart() {
+  return <h1>Carinho</h1>;
+}
+
+export default Cart;
---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Main from './pages/Home';
+import Cart from './pages/Cart';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={Main} />
+      <Route path="/cart" component={Cart} />
+    </Switch>
+  );
+}
