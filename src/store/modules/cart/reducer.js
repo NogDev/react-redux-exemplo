f0405beb5efe210c779f566596133739b9047f0d
nog <nogueira@pm.ba.gov.br>
2020-06-04 17:53:18 -0300

    alterando quantidade dos itens do carrinho

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -12,15 +12,7 @@ export default function cart(state = [], action) {
           draft.push({ ...action.product, amount: 1 });
         }
       });
-    /**
-     * vou deixar isso aqui porque eu consegui fazer sem usar a lib
-     * const productIndex = state.findIndex(p => p.id === action.product.id);
-     * if (productIndex >= 0) {
-     *  state[productIndex].amount += 1;
-     *  return state;
-     * }
-     * return [...state, { ...action.product, amount: 1 }];
-     */
+
     case '@cart/REMOVE':
       return produce(state, (draft) => {
         const productIndex = draft.findIndex((p) => p.id === action.id);
@@ -29,6 +21,19 @@ export default function cart(state = [], action) {
           draft.splice(productIndex, 1);
         }
       });
+    case '@cart/UPDATE_AMOUNT': {
+      if (action.amount <= 0) {
+        return state;
+      }
+
+      return produce(state, (draft) => {
+        const productIndex = draft.findIndex((p) => p.id === action.id);
+
+        if (productIndex >= 0) {
+          draft[productIndex].amount = Number(action.amount);
+        }
+      });
+    }
     default:
       return state;
   }
