0795c5f99fb757418efe81a21a03a75394077c16
nog <nogueira@pm.ba.gov.br>
2020-06-04 16:50:49 -0300

    removendo itens do carrinho

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -12,16 +12,23 @@ export default function cart(state = [], action) {
           draft.push({ ...action.product, amount: 1 });
         }
       });
-      //   /**
-      //  * vou deixar isso aqui porque eu consegui fazer sem usar a lib
-      //  * const productIndex = state.findIndex(p => p.id === action.product.id);
-      //  * if (productIndex >= 0) {
-      //  *  state[productIndex].amount += 1;
-      //  *  return state;
-      //  * }
-      //  * return [...state, { ...action.product, amount: 1 }];
-      //  */
+    /**
+     * vou deixar isso aqui porque eu consegui fazer sem usar a lib
+     * const productIndex = state.findIndex(p => p.id === action.product.id);
+     * if (productIndex >= 0) {
+     *  state[productIndex].amount += 1;
+     *  return state;
+     * }
+     * return [...state, { ...action.product, amount: 1 }];
+     */
+    case 'REMOVE_FROM_CART':
+      return produce(state, (draft) => {
+        const productIndex = draft.findIndex((p) => p.id === action.id);
 
+        if (productIndex >= 0) {
+          draft.splice(productIndex, 1);
+        }
+      });
     default:
       return state;
   }
