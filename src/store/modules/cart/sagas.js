7a6196a31da950317b9e273dbeb691109413c618
nog <nogueira@pm.ba.gov.br>
2020-06-05 23:21:33 -0300

    refatorando actions

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,15 +1,31 @@
 import {
- call, put, all, takeLatest
+ call, put, all, takeLatest, select
 } from 'redux-saga/effects';
 
 import api from '../../../services/api';
+import { formatPrice } from '../../../util/format';
 
-import { addToCardSuccess } from './actions';
+import { addToCardSuccess, updateAmount } from './actions';
 
 function* addToCart({ id }) {
-  const response = yield call(api.get, `/products/${id}`);
+  const productExist = yield select((state) =>
+    state.cart.find((p) => p.id === id),
+  );
 
-  yield put(addToCardSuccess(response.data));
+  if (productExist) {
+    const amount = productExist.amount + 1;
+
+    yield put(updateAmount(id, amount));
+  } else {
+    const response = yield call(api.get, `/products/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.price),
+    };
+    yield put(addToCardSuccess(data));
+  }
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
